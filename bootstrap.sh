69ec5e45df8a548eaa8f6ecbbe469d4f533af400
Aivaras Baranauskas <aivaras.baranauskas@hometogo.com>
2017-10-02 18:05:43 +0300

    routing fixed to mtch REST standarts

--- bootstrap.sh
+++ bootstrap.sh
@@ -21,6 +21,13 @@ npm install -g create-react-app
 # install php
 apt-get install -y php7.0-cli php7.0-common php7.0-fpm
 
+# install composer globally
+php -r "copy('https://getcomposer.org/installer', 'composer-setup.php');"
+php -r "if (hash_file('SHA384', 'composer-setup.php') === '544e09ee996cdf60ece3804abc52599c22b1f40f4323403c44d44fdfdd586475ca9813a858088ffbc1f233e9b180f061') { echo 'Installer verified'; } else { echo 'Installer corrupt'; unlink('composer-setup.php'); } echo PHP_EOL;"
+php composer-setup.php
+php -r "unlink('composer-setup.php');"
+mv composer.phar /usr/local/bin/composer
+
 # install mariadb
 apt-get install -y mariadb-server
 
